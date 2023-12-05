09b90bbb0bbbc073eba61d0bdb468051cd777f46
Thomas Richard <thomas.richard@farfetch.com>
2023-12-05 22:41:48 +0000

    Simplify mapping + threading is still too slow

--- day_05/compute.py
+++ day_05/compute.py
@@ -1,6 +1,9 @@
 import dataclasses
 import re
+import time
 from argparse import ArgumentParser
+from concurrent.futures import ThreadPoolExecutor
+from datetime import timedelta
 from enum import Enum
 from operator import attrgetter
 from typing import (
@@ -10,6 +13,7 @@ from typing import (
     List,
     Optional,
     Self,
+    Tuple,
 )
 
 
@@ -37,19 +41,30 @@ class Mapping:
 
     @property
     def source_end(self) -> int:
-        return self.source_start + self.length
+        return self.source_start + self.length - 1
 
     @property
     def destination_end(self) -> int:
-        return self.destination_start + self.length
+        return self.destination_start + self.length - 1
 
-    def __contains__(self, item: int) -> bool:
-        return self.source_start <= item < self.source_end
+    def in_forward(self, item: int) -> bool:
+        return self.source_start <= item <= self.source_end
+
+    def in_backward(self, item: int) -> bool:
+        return self.destination_start <= item <= self.destination_end
+
+    def reverse(self, value: int) -> Optional[int]:
+        if self.in_backward(value):
+            step = value - self.destination_start
+            return self.source_start + step
+
+        return None
 
     def convert(self, value: int) -> Optional[int]:
-        if value in self:
+        if self.in_forward(value):
             step = value - self.source_start
             return self.destination_start + step
+
         return None
 
 
@@ -59,6 +74,16 @@ class AlmanacEntry:
     destination: Subject
 
     mappings: List[Mapping]
+
+    def __post_init__(self):
+        self.mappings = sorted(self.mappings, key=attrgetter('source_start'))
+        all_mappings = sum((m.length for m in self.mappings))
+        start_mapping = min((m.source_start for m in self.mappings))
+        end_mapping = max((m.source_end for m in self.mappings))
+        print(
+            f'  Loaded Mapping({self.source.name} -> {self.destination.name}) of {all_mappings} data '
+            f'from {start_mapping} until {end_mapping} (with {len(self.mappings)} mappings)',
+        )
 
     @classmethod
     def from_file(cls, fileio: IO) -> Iterable[Self]:
@@ -80,7 +105,7 @@ class AlmanacEntry:
                         destination=current_destination,
                         # sort it by source_start in case we want to do a binary
                         # search later, and to help testing
-                        mappings=sorted(current_mappings, key=attrgetter('source_start')),
+                        mappings=current_mappings,
                     )
                 current_mappings = []  # make a new one!
                 current_source = Subject[title_match.group(1).title()]
@@ -94,18 +119,121 @@ class AlmanacEntry:
                 destination=current_destination,
                 # sort it by source_start in case we want to do a binary
                 # search later, and to help testing
-                mappings=sorted(current_mappings, key=attrgetter('source_start')),
+                mappings=current_mappings,
             )
 
-    def convert(self, value: int) -> int:
-        # TODO(tr) if too slow we could do binary search there as our mappings are
-        #  sorted by source_start
+    def get_mapping_for(self, value: int) -> Optional[Mapping]:
+        for mapping in self.mappings:
+            if mapping.in_forward(value):
+                return mapping
+        return None
+
+    def simplify(self, other: Self) -> Self:
+        if self.destination != other.source:
+            raise ValueError('Cannot simplify')
+
+        new_mappings = []
+        all_sources = set()
+        for mapping in self.mappings:
+            all_sources.add(mapping.source_start - 1)
+            all_sources.add(mapping.source_start)
+            all_sources.add(mapping.source_end)
+            all_sources.add(mapping.source_end + 1)
+
+        for mapping in other.mappings:
+            all_sources.add(self.reverse(mapping.source_start - 1))
+            all_sources.add(self.reverse(mapping.source_start))
+            all_sources.add(self.reverse(mapping.source_end))
+            all_sources.add(self.reverse(mapping.source_end + 1))
+
+        sorted_sources = sorted(all_sources)
+        for i in range(len(sorted_sources) - 1):
+            current = sorted_sources[i]
+            next_one = sorted_sources[i + 1]
+            new_mappings.append(
+                Mapping(
+                    source_start=current,
+                    destination_start=other.convert(self.convert(sorted_sources[i])),
+                    length=next_one - current,
+                ),
+            )
+
+        last = sorted_sources[-1]
+        m = self.get_mapping_for(last)
+        if m:
+            new_mappings.append(
+                Mapping(
+                    source_start=last,
+                    destination_start=other.convert(self.convert(last)),
+                    length=(m.length - last + m.source_start),
+                ),
+            )
+        else:
+            new_mappings.append(
+                Mapping(
+                    source_start=last,
+                    destination_start=other.convert(self.convert(last)),
+                    length=1,
+                ),
+            )
+
+        # simplify: remove mappings that use default
+        new_mappings = sorted(
+            [mapping for mapping in new_mappings if mapping.source_start != mapping.destination_start],
+            key=attrgetter('source_start'),
+        )
+        # simplify: remove consecutive
+        new_idx = 1
+        final_mappings = [new_mappings[0]]
+        while new_idx < len(new_mappings):
+            current = new_mappings[new_idx]
+            if final_mappings[-1].source_end == current.source_start:
+                final_mappings[-1].length += current.length
+            else:
+                final_mappings.append(current)
+            new_idx += 1
+
+        return AlmanacEntry(
+            source=self.source,
+            destination=other.destination,
+            mappings=final_mappings,
+        )
+
+    def reverse(self, value: int) -> int:
+        # brute slow conversion - not used for critical path
+        for mapping in self.mappings:
+            new_value = mapping.reverse(value)
+            if new_value is not None:
+                return new_value
+        # if not in mapping, return the value as is
+        return value
+
+    def _dumb_convert(self, value: int) -> int:
+        # boring slow conversion - when it's too small to use binary search
         for mapping in self.mappings:
             new_value = mapping.convert(value)
             if new_value is not None:
                 return new_value
         # if not in mapping, return the value as is
         return value
+
+    def convert(self, value: int) -> int:
+        # binary search
+        low = 0
+        high = len(self.mappings) - 1
+
+        while low <= high:
+            mid = (high + low) // 2
+            current = self.mappings[mid]
+
+            if (conv_value := current.convert(value)) is not None:
+                return conv_value
+            elif current.source_start < value:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return value  # not found so not transformed
 
 
 @dataclasses.dataclass
@@ -114,7 +242,7 @@ class Almanac:
     entries: Dict[Subject, AlmanacEntry]
 
     @classmethod
-    def from_file(cls, filename: str) -> Self:
+    def from_file(cls, filename: str, *, simplify: bool = False) -> Self:
         print(f'Loading {filename}')
         with open(filename, 'r') as fin:
             # first line are seeds
@@ -125,16 +253,31 @@ class Almanac:
 
             entries = {entry.source: entry for entry in AlmanacEntry.from_file(fin)}
 
-        return cls(original_seeds=original_seeds, entries=entries)
+        obj = cls(original_seeds=original_seeds, entries=entries)
+        if simplify:
+            obj = obj.simplify()
+        return obj
 
-    def unpack_seed_ranges(self) -> Iterable[List[int]]:
+    def unpack_seed_ranges(self, max_size: int = 10000000) -> Iterable[Tuple[int, int]]:
         if len(self.original_seeds) % 2 != 0:
             raise ValueError('Need an even number of seeds')
         left_seeds = list(self.original_seeds)
         while left_seeds:
             start = left_seeds.pop(0)
             n = left_seeds.pop(0)
-            yield list(range(start, start + n))
+            while n > 0:
+                yield start, min(n, max_size)
+                n -= max_size
+                start += max_size
+
+    def unpack_seeds(self) -> Iterable[int]:
+        if len(self.original_seeds) % 2 != 0:
+            raise ValueError('Need an even number of seeds')
+        left_seeds = list(self.original_seeds)
+        while left_seeds:
+            start = left_seeds.pop(0)
+            n = left_seeds.pop(0)
+            yield from range(start, start + n)
 
     def convert(self, value: int, source: Subject, destination: Subject = Subject.Location) -> int:
         if destination == source:
@@ -157,27 +300,99 @@ class Almanac:
             raise RuntimeError(f'Failed to convert {source} to {destination}')
         return current_value
 
+    def simplify(self) -> Self:
+        current = self.entries[Subject.Seed]
+        while next_step := self.entries.get(current.destination):
+            print(f'Simplifying {current.source.name}->{current.destination.name}->{next_step.destination.name}')
+            current = current.simplify(next_step)
+
+        return Almanac(
+            self.original_seeds,
+            entries={current.source: current},
+        )
+
 
 def q1(almanac: Almanac) -> int:
     return min((almanac.convert(seed, Subject.Seed, Subject.Location) for seed in almanac.original_seeds))
 
 
-def q2(almanac: Almanac) -> int:
-    overall_min = None
-    for seed_range in almanac.unpack_seed_ranges():
-        print(f'  Handling range of {len(seed_range)} seeds')
-        current_min = min((almanac.convert(seed, Subject.Seed, Subject.Location) for seed in seed_range))
-        print(f'  {current_min=} {overall_min=}')
-        if overall_min is None or current_min < overall_min:
-            overall_min = current_min
-    return overall_min
+def q2_brute(almanac: Almanac, print_at: int = 1000000) -> int:
+    almanac = almanac.simplify()  # make things slower...
+
+    current_min = None
+    unpacked_seeds_size = sum((almanac.original_seeds[2 * i + 1] for i in range(len(almanac.original_seeds) // 2)))
+    print(f'Unpacked {len(almanac.original_seeds)} to {unpacked_seeds_size} seeds')
+    start_time = last_time = time.time()
+    for i, seed in enumerate(almanac.unpack_seeds(), start=1):
+        location = almanac.convert(seed, Subject.Seed, Subject.Location)
+        if current_min is None or location < current_min:
+            current_min = location
+        if i % print_at == 0:
+            new_time = time.time()
+            current_duration = new_time - last_time
+            estimate = timedelta(seconds=(unpacked_seeds_size - i) * (current_duration / print_at))
+            print(
+                f'  after {i}/{unpacked_seeds_size} current_min={current_min} in {current_duration:0.2f}s -> estimate {estimate} left',
+            )
+            last_time = new_time
+    print(f'Computed in {time.time() - start_time:0.2f} seconds')
+    return current_min
+
+
+class ThreadedCompute:
+    def __init__(self, *, almanac: Almanac, seed_range: Tuple[int, int], name: str):
+        self.almanac = almanac
+        self.seed_range = seed_range
+        # self.found_min = None
+        self.name = name
+        self.print_at = 1000000
+        # super().__init__(*args, **kwargs)
+
+    def run(self) -> Optional[int]:
+        found_min = None
+        start_time = last_time = time.time()
+        seed_start, unpacked_seeds_size = self.seed_range
+
+        print(f' {self.name} is running for {unpacked_seeds_size} seeds')
+        for i, seed in enumerate(range(seed_start, seed_start + unpacked_seeds_size), start=1):
+            location = self.almanac.convert(seed, Subject.Seed, Subject.Location)
+            if found_min is None or location < found_min:
+                found_min = location
+            if i % self.print_at == 0:
+                new_time = time.time()
+                current_duration = new_time - last_time
+                estimate = timedelta(seconds=(unpacked_seeds_size - i) * (current_duration / self.print_at))
+                print(
+                    f' {self.name} after {i}/{unpacked_seeds_size} current_min={found_min} in {current_duration:0.2f}s -> estimate {estimate} left',
+                )
+                last_time = new_time
+
+        print(f' {self.name} completed in {time.time() - start_time:0.2f}s')
+        return found_min
+
+
+def q2_threaded(almanac: Almanac, max_workers: int = None):
+    # passing None runs with number of CPUs
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        wip = []
+        total_seeds = 0
+        for i, seed_batch in enumerate(almanac.unpack_seed_ranges()):  # type: int, Tuple[int, int]
+            total_seeds += seed_batch[1]
+            t = ThreadedCompute(
+                almanac=almanac,
+                seed_range=seed_batch,
+                name=f'compute_{i:03d}',
+            )
+            wip.append(executor.submit(t.run))
+        print(f'Created {len(wip)} tasks handled by {max_workers} workers to compute {total_seeds} seeds')
+        return min((f.result() for f in wip))
 
 
 def main(filename: str):
-    almanac = Almanac.from_file(filename)
+    almanac = Almanac.from_file(filename, simplify=True)
 
     print(f'Q1: closest location is {q1(almanac)}')
-    print(f'Q2: closest location with range {q2(almanac)}')
+    print(f'Q2: closest location with range {q2_threaded(almanac)}')
 
 
 if __name__ == '__main__':
--- day_05/test_compute.py
+++ day_05/test_compute.py
@@ -1,4 +1,5 @@
 import os
+from operator import attrgetter
 
 import pytest
 
@@ -8,7 +9,8 @@ from day_05.compute import (
     Mapping,
     Subject,
     q1,
-    q2,
+    q2_brute,
+    q2_threaded,
 )
 
 
@@ -29,22 +31,39 @@ def input_txt():
 
 
 class TestAlmanacEntry:
+    seed_to_soil = (
+        (0, 0),
+        (1, 1),
+        (48, 48),
+        (49, 49),
+        (50, 52),
+        (51, 53),
+        (96, 98),
+        (97, 99),
+        (98, 50),
+        (99, 51),
+    )
+
     @pytest.mark.parametrize(
         'seed, soil',
-        (
-            (0, 0),
-            (1, 1),
-            (48, 48),
-            (49, 49),
-            (50, 52),
-            (51, 53),
-            (96, 98),
-            (97, 99),
-            (98, 50),
-            (99, 51),
-        ),
+        seed_to_soil,
     )
     def test_convert(self, seed, soil):
+        almanac_entry = AlmanacEntry(
+            source=Subject.Seed,
+            destination=Subject.Soil,
+            mappings=sorted(
+                [
+                    Mapping(50, 98, 2),
+                    Mapping(52, 50, 48),
+                ],
+                key=attrgetter('source_start'),
+            ),
+        )
+        assert almanac_entry._dumb_convert(seed) == almanac_entry.convert(seed) == soil
+
+    @pytest.mark.parametrize('seed, soil', seed_to_soil)
+    def test_revert(self, seed, soil):
         almanac_entry = AlmanacEntry(
             source=Subject.Seed,
             destination=Subject.Soil,
@@ -53,7 +72,7 @@ class TestAlmanacEntry:
                 Mapping(52, 50, 48),
             ],
         )
-        assert almanac_entry.convert(seed) == soil
+        assert almanac_entry.reverse(soil) == seed
 
     def test_original_seed_to_soil(self, small_ex_txt):
         almanac = Almanac.from_file(small_ex_txt)
@@ -97,9 +116,43 @@ class TestAlmanac:
     def test_unpack_seed_ranges(self, small_ex_txt):
         almanac = Almanac.from_file(small_ex_txt)
         assert list(almanac.unpack_seed_ranges()) == [
-            list(range(79, 93)),
-            list(range(55, 68)),
+            (79, 93 - 79),
+            (55, 68 - 55),
         ]
+
+    def test_unpack_seeds(self, small_ex_txt):
+        almanac = Almanac.from_file(small_ex_txt)
+        assert list(almanac.unpack_seeds()) == list(range(79, 93)) + list(range(55, 68))
+
+    @pytest.mark.parametrize('input_simplify, simplify_with', ((Subject.Seed, Subject.Soil),))
+    def test_simplify(self, small_ex_txt, input_simplify, simplify_with):
+        almanac = Almanac.from_file(small_ex_txt)
+        seed_entry = almanac.entries[input_simplify]
+        st = min((m.source_start for m in seed_entry.mappings))
+        ed = max((m.source_end for m in seed_entry.mappings))
+
+        other_entry = almanac.entries[simplify_with]
+        output_simplify = other_entry.destination
+
+        print(
+            f'Simplifying from {seed_entry.source}->{seed_entry.destination} with {other_entry.source}->{other_entry.destination}',
+        )
+        simpler = seed_entry.simplify(other_entry)
+        assert simpler.source == seed_entry.source == input_simplify
+        assert simpler.destination == other_entry.destination == output_simplify
+
+        st = min((st, min((seed_entry.reverse(m.source_start) for m in other_entry.mappings))))
+        ed = max((ed, max((seed_entry.reverse(m.source_end) for m in other_entry.mappings))))
+
+        assert simpler.destination == other_entry.destination == output_simplify
+        if simplify_with == Subject.Soil:
+            assert st == 0, 'sanity'
+            assert ed == 99, 'sanity'
+
+        for i in range(st, ed + 1):
+            v = simpler.convert(i) == other_entry.convert(seed_entry.convert(i))
+            if not v:
+                assert v, f'For {i}->{simpler.convert(i)} vs {i}->{seed_entry.convert(i)}->{other_entry.convert(seed_entry.convert(i))}'
 
 
 class TestQ1:
@@ -111,5 +164,8 @@ class TestQ1:
 
 
 class TestQ2:
-    def test_small_ex(self, small_ex_txt):
-        assert q2(Almanac.from_file(small_ex_txt)) == 46
+    def test_small_ex_brute(self, small_ex_txt):
+        assert q2_brute(Almanac.from_file(small_ex_txt)) == 46
+
+    def test_small_ex_threaded(self, small_ex_txt):
+        assert q2_threaded(Almanac.from_file(small_ex_txt)) == 46
